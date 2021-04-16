ebf0d04646ff1573bbef8c7607b5b8f52ad0af66
Drantho <drantho@gmail.com>
2021-04-16 16:01:36 -0700

    passport.js setup

--- Passport/passport.js
+++ Passport/passport.js
@@ -1,14 +1,44 @@
-const passport = require('pass');
-const FacebookStrategy = require('passport-facebook');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const JwtStrategy = require('passport-jwt').Strategy;
+const User = require('../models/User');
 
-passport.use(new FacebookStrategy({
-    clientID: process.env.FACEBOOK_ID,
-    clientSecret: process.env.FACEBOOK_SECRET,
-    callbackUrl: '/auth/facebook/callback'
-},
-(accessToken, refreshToken, profile, cb)=>{
-    console.log(profile);
+const cookieExtractor = req => {
+    let token = null;
+    if(req && req.cookies){
+        token = req.cookies["access_token"];
+    }
+    return token;
+}
 
-    return cb(null, profile);
-}));
+passport.use(new JwtStrategy({
+    jwtFromRequest: cookieExtractor,
+    secretOrKey: process.env.JWT_SECRET
+}, (payload, done) => {
+    User.findById({_id: payload.sub}, (error, user) => {
+        if(error){
+            return done(error, false);
+        }
+
+        if(user){
+            return done(null, user);
+        }
+
+        return done(null, false);
+    })
+}))
+
+passport.use(new LocalStrategy((username, password, done)=>{
+    User.findOne({username}, (error, user) => {
+        if(error){
+            return done(error);
+        }
+
+        if(!user){
+            return done(null, false);
+        }
+
+        user.comparePassword(password, done);
+    })
+}))
 
