507f27ff8fc7238c6a2f75b0b6a0067bc4c196e8
Drantho <drantho@gmail.com>
2021-04-16 14:49:00 -0700

    mongodb connected

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const compression = require('compression');
 const passport = require('passport');
 const FacebookStrategy = require('passport-facebook');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 
@@ -13,9 +14,10 @@ app.use(cors());
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.json({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 
-const PORT = process.env.PORT || 3001;
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {useNewUrlParser: true}, () => {
+    console.log(`mongodb connected.`);
+});
 
-app.use(passport.initialize()); 
-require("./config/passport");
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, ()=> console.log(`server running on port ${PORT}`))
